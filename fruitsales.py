f55074e073b2dbce4eff2ec089b302c59a08c0b8
Bret Shroats <bshroats5@gmail.com>
2023-12-10 13:35:17 -0500

    Created the dictionary

--- fruitsales.py
+++ fruitsales.py
@@ -1,2 +1,11 @@
 # add your code here
+# importing pandas
 import pandas as pd
+
+# Creating a fruit sales dictionary
+
+data = {
+   'Apples': ['2017 Sales', '2018 Sales'],
+   'Bananas': [35, 41]
+}
+
